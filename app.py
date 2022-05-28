f9de5b39c716df1b5127e29dc92e1d70a7f799dc
GoodnessObi <goodnessobi@gmail.com>
2022-05-28 22:39:19 +0100

    lists out artists present in the database

--- app.py
+++ app.py
@@ -294,16 +294,20 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+  # data=[{
+  #   "id": 4,
+  #   "name": "Guns N Petals",
+  # }, {
+  #   "id": 5,
+  #   "name": "Matt Quevedo",
+  # }, {
+  #   "id": 6,
+  #   "name": "The Wild Sax Band",
+  # }]
+
+  data=Artist.query.with_entities(Artist.id, Artist.name).all()
+  # data=Artist.query.options(load_only('email', 'id')).all()
+  print('data>>>', data)
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
