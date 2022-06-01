2b3aeffcbf04dc785b8b48690bbaaaecfd0d846f
GoodnessObi <goodnessobi@gmail.com>
2022-06-01 21:33:30 +0100

    display venue and artist details edit venue and artist

--- static/js/controllers/index.js
+++ static/js/controllers/index.js
@@ -1,12 +1,20 @@
 
-import { addNewVenue } from './new_venue.js';
-import { addNewArtist } from './new_artist.js';
+import { addNewVenue, updateVenue } from './venue.js';
+import { addNewArtist, updateArtist } from './artist.js';
 
 if (document.getElementById('venue-form')) {
   document.getElementById('venue-form').onsubmit = addNewVenue;
 }
 
+if (document.getElementById('update-venue-form')) {
+  document.getElementById('update-venue-form').onsubmit = updateVenue;
+}
+
 if (document.getElementById('artist-form')) {
   document.getElementById('artist-form').onsubmit = addNewArtist;
+}
+
+if (document.getElementById('update-artist-form')) {
+  document.getElementById('update-artist-form').onsubmit = updateArtist;
 }
 
